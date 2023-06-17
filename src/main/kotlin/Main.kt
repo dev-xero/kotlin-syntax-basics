3cb7da149c5eed93d7a9a044ddcb73b3821845a8
Xero kun <elo48858@gmail.com>
2023-06-17 09:30:47 +0100

    `FEAT` Implemented basic functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,20 @@
 fun main(args: Array<String>) {
     // this function is just like Java's "main()" method, it accepts an array of strings
-    println("Hello, again kotlin...")
+    printMsg("Hello, Kotlin")
+    printMsg("This is pretty cool")
+    printMsg(square(3))
+}
+
+fun printMsg(msg: String) {
+    println(msg)
+}
+
+fun printMsg(msg: Int) {
+    // function overloading
+    println(msg)
+}
+
+fun square(num: Int): Int {
+    // returns an "Int" data type
+    return num * num
 }
