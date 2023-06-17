89a184b8dee7e44a61e478e0b06be893612618f0
Xero kun <elo48858@gmail.com>
2023-06-17 09:44:18 +0100

    `FEAT` Implemented linear search

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,37 @@
 fun main() {
-    // this function is just like Java's "main()" method, it accepts an array of strings
+    // Testing
     printMsg("Hello, Kotlin")
     printMsg("This is pretty cool")
     printMsg(square(3))
+
+    val testArray = arrayOf(1, 2, 3, 4, 5, 6)
+    println(testArray.contentToString())
+    print("The index of \"2\" is: ")
+    printMsg(linearSearch(testArray, 2))
 }
 
 fun printMsg(msg: String) {
+    // Prints a string to standard output
     println(msg)
 }
 
 fun printMsg(msg: Int) {
-    // function overloading
+    // Overloaded: prints an integer to standard output
     println(msg)
 }
 
 fun square(num: Int): Int {
-    // returns an "Int" data type
+    // Returns the square of an integer number
     return num * num
+}
+
+fun linearSearch(theArray: Array<Int>, key: Int): Int {
+    // Simple linear search
+    for (i in 0..theArray.size) {
+        if (theArray[i] == key) {
+            return i
+        }
+    }
+
+    return -1
 }
